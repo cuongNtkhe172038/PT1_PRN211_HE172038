63fcb4ac68fb2e557b6df139a66305fd98aa4da7
cuongNtkhe172038 <cuong040403@gmail.com>
2024-01-24 08:40:18 +0700

    assignment 1

---
+++ Ass01Solution/DataAccess/Repository/IMemberRepository.cs
@@ -0,0 +1,22 @@
+﻿using BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    internal interface IMemberRepository
+    {
+        public MemberObject GetMemberByID(int id);
+        public MemberObject GetMemberByEmail(string email);
+        public IEnumerable<MemberObject> GetMembersList();
+        public void InserMember(MemberObject member);
+        public void UpdateMember(MemberObject member);
+        public void DeleteMember(int memberID);
+        public void SortMemberByID();
+        public void SortMemberByName();
+        public void SearchById(string id);
+    }
+}
---
+++ factoriesdemo/factoriesdemo/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace factoriesdemo
+{
+    // Interface định nghĩa chung cho các hình học
+    public interface IShape
+    {
+        void Draw();
+    }
+
+    // Các lớp cụ thể triển khai IShape
+    public class Circle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing a Circle");
+        }
+    }
+
+    public class Square : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing a Square");
+        }
+    }
+
+    // Abstract class cho Factory
+    public abstract class ShapeFactory
+    {
+        public abstract IShape CreateShape();
+    }
+
+    // Các lớp Factory triển khai ShapeFactory
+    public class CircleFactory : ShapeFactory
+    {
+        public override IShape CreateShape()
+        {
+            return new Circle();
+        }
+    }
+
+    public class SquareFactory : ShapeFactory
+    {
+        public override IShape CreateShape()
+        {
+            return new Square();
+        }
+    }
+
+    // Client sử dụng Factory để tạo đối tượng
+    public class Client
+    {
+        public void DrawShape(ShapeFactory shapeFactory)
+        {
+            IShape shape = shapeFactory.CreateShape();
+            shape.Draw();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Client client = new Client();
+
+            // Sử dụng CircleFactory để tạo hình tròn
+            client.DrawShape(new CircleFactory());
+
+            // Sử dụng SquareFactory để tạo hình vuông
+            client.DrawShape(new SquareFactory());
+        }
+    }
+}
